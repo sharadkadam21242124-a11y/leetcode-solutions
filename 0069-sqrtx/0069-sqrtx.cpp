b37f46827e385cc24365b3181405247606ff4654
sharadkadam21242124-a11y <sharadkadam21242124@gmail.com>
2026-07-27 04:12:30 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.85%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -4,9 +4,25 @@ public:
         if(x==0){
             return 0;
         }
-        for(int i=1;i<46340;i++){
-            if(x>=(i*i) && x<((i+1)*(i+1))){
-                return i;
+    //     for(int i=1;i<46340;i++){
+    //         if(x>=(i*i) && x<((i+1)*(i+1))){
+    //             return i;
+    //         }
+    //     }
+
+        int str=1;
+        int end=46339;
+        int mid;
+        while(str<=end){
+            mid=str + (end-str)/2;
+            if(x>=(mid*mid) && x<((mid+1)*(mid+1))){
+                return mid;
+            }
+            else if(x<(mid*mid)){
+                end=mid-1;
+            }
+            else{
+                str=mid+1;
             }
         }
         return 46340;
